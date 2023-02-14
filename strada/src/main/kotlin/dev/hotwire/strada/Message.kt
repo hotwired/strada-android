6a8d7d348a2767df4b47ecc54c6e75b055b76b7a
Jay Ohms <jay@37signals.com>
2023-02-14 09:40:39 -0500

    Clean up import

--- strada/src/main/kotlin/dev/hotwire/strada/Message.kt
+++ strada/src/main/kotlin/dev/hotwire/strada/Message.kt
@@ -3,7 +3,6 @@ package dev.hotwire.strada
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.buildJsonObject
