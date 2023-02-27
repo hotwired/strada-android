fd7c5c7386da5b17f2b0ccfc4deb294c4f687c8a
Jay Ohms <jay.ohms@gmail.com>
2023-02-27 15:22:33 -0500

    Remove bridgeComponentFactory() function, rely on constructor

--- strada/src/main/kotlin/dev/hotwire/strada/BridgeComponentFactory.kt
+++ strada/src/main/kotlin/dev/hotwire/strada/BridgeComponentFactory.kt
@@ -1,13 +1,8 @@
 package dev.hotwire.strada
 
-class BridgeComponentFactory<in D : BridgeDelegate, out C : BridgeComponent> internal constructor(
+class BridgeComponentFactory<in D : BridgeDelegate, out C : BridgeComponent> constructor(
     val name: String,
     private val creator: (name: String, delegate: D) -> C
 ) {
     fun create(delegate: D) = creator(name, delegate)
 }
-
-fun <D : BridgeDelegate, C : BridgeComponent> bridgeComponentFactory(
-    name: String,
-    creator: (name: String, delegate: D) -> C
-) = BridgeComponentFactory(name, creator)
