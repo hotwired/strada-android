097ff339a041dd7cbcf2549bd93be54ea5386628
Jay Ohms <jay.ohms@gmail.com>
2023-07-12 09:59:30 -0400

    Implement the BridgeDelegate as a lifecycle observer to avoid complicated fragment view lifecycle timing issues

--- strada/src/main/kotlin/dev/hotwire/strada/BridgeDestination.kt
+++ strada/src/main/kotlin/dev/hotwire/strada/BridgeDestination.kt
@@ -1,9 +1,6 @@
 package dev.hotwire.strada
 
-import androidx.lifecycle.LifecycleOwner
-
 interface BridgeDestination {
     fun bridgeDestinationLocation(): String
-    fun bridgeDestinationLifecycleOwner(): LifecycleOwner
     fun bridgeWebViewIsReady(): Boolean
 }
